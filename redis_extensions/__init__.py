b818a3c0e5b31bc358d54e5051d13a51816aa1eb
Brightcells <brightcells@gmail.com>
2016-08-26 17:18:08 +0800

    class StrictRedisExtensions(StrictRedis)

--- redis_extensions/__init__.py
+++ redis_extensions/__init__.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from redis_extensions.extensions import (
-    get_delete,
-    get_rename,
-    multi_pop,
-    trim_lpush,
-    trim_rpush,
-    rawscore,
-    zaddwithstamps,
-    zincrbywithstamps,
+    StrictRedisExtensions
 )
--- redis_extensions/extensions.py
+++ redis_extensions/extensions.py
@@ -2,83 +2,75 @@
 
 import time
 
+from redis import StrictRedis
 from redis._compat import iteritems
 
 
-class RedisExtensions(object):
+class StrictRedisExtensions(StrictRedis):
 
-    def __init__(self):
-        self.rate = 10 ** 13
-        self.max_timestamp = 9999999999999
+    def __new__(cls, *args, **kwargs):
+        # Private Var
+        cls.__rate = 10000000000000  # 10 ** 13,
+        # Private Var
+        cls.__max_timestamp = 9999999999999
+        return super(StrictRedisExtensions, cls).__new__(cls, *args, **kwargs)
 
-    def get_delete(self, conn, key):
-        pipe = conn.pipeline()
+    def get_delete(self, key):
+        pipe = self.pipeline()
         pipe.get(key)
         pipe.delete(key)
         return pipe.execute()
 
-    def get_rename(self, conn, key, suffix='del'):
-        pipe = conn.pipeline()
+    def get_rename(self, key, suffix='del'):
+        pipe = self.pipeline()
         pipe.get(key)
-        pipe.rename(key, '{}_{}'.format(key, suffix)) if conn.exists(key) else pipe.exists(key)
+        pipe.rename(key, '{}_{}'.format(key, suffix)) if self.exists(key) else pipe.exists(key)
         return pipe.execute()
 
-    def multi_pop(self, conn, key, num):
+    def multi_pop(self, key, num):
         if num <= 0:
             return [[], False, 0]
-        pipe = conn.pipeline()
+        pipe = self.pipeline()
         pipe.lrange(key, 0, num - 1)
         pipe.ltrim(key, num, -1)
         pipe.llen(key)
         return pipe.execute()
 
-    def trim_lpush(self, conn, key, num, *values):
-        pipe = conn.pipeline()
+    def trim_lpush(self, key, num, *values):
+        pipe = self.pipeline()
         pipe.lpush(key, *values)
         pipe.ltrim(key, 0, num - 1)
         pipe.llen(key)
         return pipe.execute()
 
-    def trim_rpush(self, conn, key, num, *values):
-        pipe = conn.pipeline()
+    def trim_rpush(self, key, num, *values):
+        pipe = self.pipeline()
         pipe.rpush(key, *values)
         pipe.ltrim(key, -num, - 1)
         pipe.llen(key)
         return pipe.execute()
 
-    def timestamps(self, desc=False):
+    # Private Function
+    def __timestamps(self, desc=False):
         stamp = int(time.time() * 1000)
-        return self.max_timestamp - stamp if desc else stamp
+        return self.__max_timestamp - stamp if desc else stamp
 
     def rawscore(self, score):
         if not score:
             return 0.0
-        return float(int(float(score) / self.rate))
+        return float(int(float(score) / self.__rate))
 
-    def stampscore(self, score, desc=False):
-        return score * self.rate + self.timestamps(desc)
+    # Private Function
+    def __stampscore(self, score, desc=False):
+        return score * self.__rate + self.__timestamps(desc)
 
-    def zaddwithstamps(self, conn, name, *args, **kwargs):
+    def zaddwithstamps(self, name, *args, **kwargs):
         desc = 'desc' in kwargs and kwargs.pop('desc')
-        pieces = [item if index % 2 else self.stampscore(item, desc) for index, item in enumerate(args)]
+        pieces = [item if index % 2 else self.__stampscore(item, desc) for index, item in enumerate(args)]
         for pair in iteritems(kwargs):
-            pieces.append(self.timestamps(pair[1], desc))
+            pieces.append(self.__timestamps(pair[1], desc))
             pieces.append(pair[0])
-        return conn.zadd(name, *pieces)
+        return self.zadd(name, *pieces)
 
-    def zincrbywithstamps(self, conn, name, value, amount=1, desc=False):
-        return conn.zadd(name, self.stampscore(self.rawscore(conn.zscore(name, value)) + amount, desc), value)
-
-_global_instance = RedisExtensions()
-
-# String
-get_delete = _global_instance.get_delete
-get_rename = _global_instance.get_rename
-# List
-multi_pop = _global_instance.multi_pop
-trim_lpush = _global_instance.trim_lpush
-trim_rpush = _global_instance.trim_rpush
-# Sorted Set
-rawscore = _global_instance.rawscore
-zaddwithstamps = _global_instance.zaddwithstamps
-zincrbywithstamps = _global_instance.zincrbywithstamps
+    def zincrbywithstamps(self, name, value, amount=1, desc=False):
+        return self.zadd(name, self.__stampscore(self.rawscore(self.zscore(name, value)) + amount, desc), value)
