d9c0af756c7c67e32165e6bd543e8a166bcdb5d1
huangqimin001 <huangqimin001@ke.com>
2022-10-21 16:02:01 +0800

    :arrow_up: Up python require

--- setup.py
+++ setup.py
@@ -19,14 +19,13 @@ setup(
 
     packages=['redis_extensions'],
     py_modules=[],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=['CodeConvert', 'TimeConvert', 'redis>=4.0.2', 'shortuuid', 'verification-code', 'graphic-verification-code>=1.0.3'],
 
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
