6d37e40a09c6e65f871669c9ed54f6c96cdfe372
huangqimin001 <huangqimin001@ke.com>
2023-01-29 18:25:53 +0800

    :art: StrictRedisExtensions = RedisExtensions

--- redis_extensions/__init__.py
+++ redis_extensions/__init__.py
@@ -2,7 +2,7 @@ import redis
 from redis import *
 
 from redis_extensions.expires import BaseRedisExpires, RedisExpires
-from redis_extensions.extensions import StrictRedisExtensions
+from redis_extensions.extensions import RedisExtensions, StrictRedisExtensions
 
 
-__all__ = redis.__all__ + ['StrictRedisExtensions', 'BaseRedisExpires', 'RedisExpires']
+__all__ = redis.__all__ + ['RedisExtensions', 'StrictRedisExtensions', 'BaseRedisExpires', 'RedisExpires']
